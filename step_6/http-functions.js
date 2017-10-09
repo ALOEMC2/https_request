4f305323fe51af8c08c320c6e3a02a93b1fd60e7
Al OFlaherty <aloemc2@gmail.com>
2017-10-09 23:35:26 +0000

    step 6 completed

---
+++ step_6/http-functions.js
@@ -0,0 +1,35 @@
+
+
+var https = require('https');
+
+    /* Your code here */
+
+ function getHTML (options, callback) {
+  var buffer="";
+  https.get(options, function (response) {
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      console.log('Chunk Received. Length:', data.length);
+      console.log('[-- CHUNK OF LENGTH ' + data.length + ' --]');
+      buffer += data;
+
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+      callback(buffer)
+    });
+  });
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+module.exports = getHTML;
